c33e8a83c84329af66e12b7735d112334af0f7b0
Noah067 <abelbenitessales@gmail.com>
2024-09-23 15:15:10 -0400

    First commit

---
+++ teste.py
@@ -0,0 +1,90 @@
+ #print(42000)
+# print(42,000)
+# print(42.000)
+
+# print(type(42000))
+# print(type(42.000))
+# print(type(42))
+
+
+# ---------------------------------------------------
+
+
+
+# print("10")
+# print(type("10"))
+# print("3.14")
+# print(type("3.14"))
+
+
+# ----------------------------------------------------
+
+
+    # print(type('Esse e um string.'))
+    # print(type("E esse tambem w um string."))
+    # print(type(""" esse tambem."""))
+    # print(type('''e mesmo esse...'''))
+
+
+#-------------------------------------------------------
+
+
+#  Peso = 70
+#  Altura = 1.79
+#  IMC = (Peso/Altura**2)
+
+#  print (IMC)
+
+
+#------------------------------------------------------
+
+
+
+# nome= input("Digite seu nome = ")
+# print("Seu nome é = ",nome)
+
+
+#------------------------------------------------------
+
+
+# salario = int(input("salario = "))
+# imposto=float(input("digite o imposto = "))
+
+
+#------------------------------------------------------
+
+
+# sal = int(input("Salario = "))
+# print("O salário digitaDO foi = " %sal)
+
+
+#------------------------------------------------------
+
+
+# print("+" + 10*"-" + "+")
+# print(("|"+" "*10+"|\n") * 5, end="")
+# print("+"+ 10*"-"+"+")
+
+#------------------------------------------------------
+
+# print(" "*5 + "/" + "\\ ")
+# print(" "*4 + "/" + " "*2+"\\     <-------- triângulo das bermudas")
+# print(" "*3 + "/" + " "*4+"\\ ")
+# print(" "*2 + "/" + " "*6+"\\ ")
+# print(" "*1 + "/" + " "*8+"\\ ")
+# print("/" + 10*"_" + "\\ ")
+
+
+#-------------------------------------------------------
+#forma mais avançada de formatação de strings
+
+
+# frase= 'Um triângulo de base igual a {0} e altura igual a {1} possui area igual a {2}.'.format(3, 4, 12)
+
+# print(frase)
+
+
+linguagem = "Python"
+
+
+
