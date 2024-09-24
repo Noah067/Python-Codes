98ba48ae780c7b3d2137f31b2f59364070cac12d
Noah067 <abelbenitessales@gmail.com>
2024-09-24 17:36:30 -0400

    Aula do dia 24 de setembro

---
+++ aulaDia24setembro.py
@@ -0,0 +1,117 @@
+# # q = int
+# # v = float
+# # p = float
+# # valordacompra = int
+
+# # nome = str(input("Digite o nome do produto = "))
+
+# # q = int(input("Digite a quantidade de produto = "))
+
+# # v = float(input("Digite o valor do seu produto = "))
+
+# # p = float(input("Digite o percentual = "))
+
+# # total = q*v
+# # valordacompra = total - total*p/100
+
+
+# # print(valordacompra)
+
+# #------------------------------------------------------------------------------------
+
+# # dolar = float
+
+# # reais =float
+
+# # restodomoney = float
+
+
+# # reais = float(input("Digite quantos reais o senhor(a) deseja trocar = "))
+
+# # dolar = float(input("O valor do dolar esta custando = "))
+
+
+# # restodomoney = reais/dolar
+
+# # print("seu dinheiro sera de = ",restodomoney,"dolares")
+
+
+# #-----------------------------------------------------------------------------------------
+
+
+
+
+# # nome = str(input("Digite seu nome = "))
+
+# # idade = float(input("Digite sua idade = "))
+
+# # sexo = str(input("Escreva seu sexo(Masculino ou Feminino):  = "))
+
+# # nota1 = float(input("Digite sua nota do 1º bimestre = "))
+
+# # nota2 = float(input("Digite sua nota do 2º bimestre = "))
+
+# # media = (nota1+nota2)/2
+
+# # print("Sua media sera de = ",media)
+
+
+# #-----------------------------------------------------------------------------------------
+
+
+# # valor1 = float(input("Digite um valor = "))
+
+# # valor2 = float(input("Digite um valor = "))
+
+# # valor3 = float(input("Digite um valor = "))
+
+# # media = (valor1+valor2)*valor3
+
+# # somadosvalores = valor1+valor2
+
+# # print("A soma dos 2 primeiros valores sera de = ",somadosvalores)
+
+# # print("Seu valor final  sera = ",media)
+
+
+
+# #-----------------------------------------------------------------------------------------
+
+
+
+# # metros = float(input("Digite quantos metros = "))
+
+# # metrosemcentimetros = metros*100
+
+# # print("Centimetros = ",metrosemcentimetros)
+
+
+
+# #-------------------------------------------------------------------------------------
+
+
+
+# nome = str(input("Digite seu nome = "))
+# cpf = int(input("Digite seu CPF = "))
+# rg = int(input("Digite seu RG = "))
+# datadenascimento = int(input("Digite sua data de nascimento = "))
+# sexo = str(input("Digite seu sexo = "))
+# peso = float(input("Digite seu peso = "))
+# tiposanguineo = str(input("Digite seu tipo sanguineo = "))
+# renda = float(input("Digite sua renda = "))
+# endereco = str(input("Digite seu endereço = "))
+# telefone = int(input("Digite seu telefone = "))
+# cidadeeestado = str(input("Digite sua cidade e estado = "))
+
+# print("Seus Dados : ")
+# print(nome)
+# print(cpf)
+# print(rg)
+# print(datadenascimento)
+# print(sexo)
+# print(peso)
+# print(tiposanguineo)
+# print(renda)
+# print(endereco)
+# print(telefone)
+# print(cidadeeestado)
