3ab75b13df51954ca68ffe2085d8eda8b2b18ba8
Noah067 <abelbenitessales@gmail.com>
2024-10-22 18:08:51 -0300

    aulaDia22outubro

---
+++ auldaDia22outubro.py
@@ -0,0 +1,57 @@
+# pais_a = 80.000
+# pais_b = 200.000
+# cont = 0
+
+# while pais_a<=pais_b:
+#     pais_a = pais_a+(pais_a*3/100)
+#     pais_b = pais_b+(pais_b*1.5/100)
+#     cont = cont+1
+# print (cont)
+
+
+
+
+
+
+# x = float(input("Digite a taxa de crescimento da emrpresa A = "))
+# z = float(input("Digite a taxa de crescimento da emrpresa B = "))
+# a = float(input("DIgite a quantidade da população A ="))
+# b = float(input("DIgite a quantidade da população B ="))
+# cont = 0
+
+# while True:
+
+#     a = a+(a*x)/100
+#     b = b+(b*z)/100
+#     cont = cont+1
+
+#     if a >= b:
+#        break
+# print(cont)
+
+
+
+
+
+# while True:
+#     total = 0
+#     while True:
+
+#             x = float(input("Digite o valor do produto = "))
+
+#             total = total + x
+#             print(total)
+#             if x == 0.0:
+#                 break
+#             produto = str(input("Digite o nome do produto = "))
+#             print(produto,":", x)
+#     print(total)
+#     while True:
+            
+#         money = float(input("Dinheiro recebido do cliente = "))
+#         if money >= total:
+#                     troco = money - total
+#                     print("Seu troco é = ",troco)
+#                     break
+#         else:
+#                     print("Valor insuficiente(VAI EMBORA SEU POBRE)")
