d31b8a7786cea305c6aa043daa95a4c1304f1013
Noah067 <abelbenitessales@gmail.com>
2024-10-24 18:17:53 -0300

    aulaDia24outubro

---
+++ aulaDia24outubro.py
@@ -0,0 +1,24 @@
+# nomes = ["Pedro", "João", "Leticia"]
+# for n in nomes:
+#     if n == "João":
+#         continue
+#     print(n)
+
+
+
+# for j in range (10):
+#     print(j)
+
+
+
+# nomes = ["Marcos", "Pedro", "2004", "Administrador", "515.651.165-21"]
+# for i in range(7):
+#     print(i,nomes[3])
+
+
+
+
+# number = int(input("Digite um número = "))
+# for i in range(1,11):
+#     print(f"{number} x {i} = {number*i}")
+    
