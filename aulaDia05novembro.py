9968c6509bfdef998088a964abc575ed64977179
Noah067 <abelbenitessales@gmail.com>
2024-11-07 18:35:23 -0300

    aulaDia07novembro

---
+++ aulaDia05novembro.py
@@ -0,0 +1,17 @@
+# tradutor = {}
+# tradutor = ['pineapple'] = 'Abacaxi'
+# tradutor = ['apple'] = 'Maça'
+# tradutor = ['orange'] = 'laranja'
+# print(type(tradutor))
+# print(tradutor)
+
+
+
+
+
+while True:
+    try:
+        print('1- multiplicacao')
+        print('2- Divisão')
+        print('3- Subtração')
+        print('4- Adição')
---
+++ aulaDia07novembro.py
@@ -0,0 +1,87 @@
+# def hello():
+#     print("ola")
+# hello()
+
+
+#---------------------------------------------------------
+
+
+# def hello (nome):
+#     print("ola" , nome)
+#     hello("pjl")
+
+
+    #---------------------------------------
+
+
+
+# def hello (nome1):
+#     print("seja bem vindo", nome1)
+
+#     a = input("digite seu nome = ")
+
+
+#--------------------------------------------------------------------
+
+
+
+
+    # def hi (nome2, idade):
+    #     print("ola", nome2, "\n su0a idade é = ", idade)
+
+    #     c = input("digite seu nome = ")
+    #     b = input ("digite sua idade = ")
+    #     hi(c,b)
+
+
+
+#-------------------------------------------------------------------
+
+
+
+# def calcular_pag (qts_horas, valor_hora):
+
+#     horas = float(input("Digite as horas = " ,qts_horas))
+#     taxa = float(input("Digite a taxa = ", valor_hora))
+#     if horas <= 40:
+#         salario=horas*taxa
+#     else:
+#         h_excd = horas - 40
+#         salario = 40*taxa+(h_excd*(1.5*taxa))
+#         print(salario)
+
+
+
+#----------------------------------------------------------------
+
+
+
+# def soma(x, y):
+#     result = x+y
+#     return result
+
+# a = int(input("Primeiro numero = "))
+# b = int(input("Segundo numero = "))
+# result = soma(a,b)
+# print("soma", result)
+
+
+
+#----------------------------------------------------------------
+
+
+
+# def inverte(nome, sobrenome):
+#     nomeinverso = sobrenome+",",nome
+#     return nomeinverso
+# nome = input("nome = ")
+# sobrenome = input("sobrenome = ")
+# invertido = inverte(nome, sobrenome)
+# print("ola", invertido)
+
+
+
+#------------------------------------------------------------------
+
+
+
